fc9328d9593796f4cf21c83f05da8121a2307acc
TheB4ssPl4y3r <ramon.rohner@gmx.ch>
2021-10-28 10:29:13 +0200

    did some changes

--- Lin_Rohner_lab2/pos_vel_acc_plot.m
+++ Lin_Rohner_lab2/pos_vel_acc_plot.m
@@ -12,7 +12,7 @@ d_pos = designfilt('lowpassfir', 'FilterOrder', 2, 'CutoffFrequency', .125, 'Sam
 %design lowpassfilter for Velocity
 d_vel = designfilt('lowpassfir', 'FilterOrder', 2, 'CutoffFrequency', .125, 'SampleRate', 10);
 %design lowpassfilter for Acceleration
-d_acc = designfilt('lowpassfir', 'FilterOrder', 2, 'CutoffFrequency', .125, 'SampleRate', 10);
+d_acc = designfilt('lowpassfir', 'FilterOrder', 2, 'CutoffFrequency', 5, 'SampleRate', 10);
 
 %offline filtering
 pos_filt_off = filtfilt(d_pos, pos_raw);
@@ -20,11 +20,11 @@ vel_filt_off = filtfilt(d_vel, vel_raw);
 acc_filt_off = filtfilt(d_acc, acc_raw);
 
 %Position plot
-plot(time,pos_raw, time, pos_filt_on, time, pos_filt_off);
-title('Position [deg] measured by Hall-Sensor raw / online-filtered / offline-filtered');
-xlabel('Time [s]');
-ylabel('Position [deg]');
-legend('Position raw','Position online-filtered', 'Position offline-filtered');
+% plot(time,pos_raw, time, pos_filt_on, time, pos_filt_off);
+% title('Position [deg] measured by Hall-Sensor raw / online-filtered / offline-filtered');
+% xlabel('Time [s]');
+% ylabel('Position [deg]');
+% legend('Position raw','Position online-filtered', 'Position offline-filtered');
 
 % %Velocity plot
 % plot(time, vel_raw, time, vel_filt_on, time, vel_filt_off);
@@ -33,9 +33,9 @@ legend('Position raw','Position online-filtered', 'Position offline-filtered');
 % ylabel('Velocity [deg/s]');
 % legend('Velocity raw','Velocity online-filtered', 'Velocity offline-filtered');
 % 
-% %Acceleration plot
-% plot(time, acc_raw, time, acc_filt_on, acc_filt_off);
-% title('Acceleration [deg/s^2] measured by Hall-Sensor raw / online-filtered / offline-filtered');
-% xlabel('Time [s]');
-% ylabel('Acceleration [deg/s^2]');
-% legend('Acceleration raw','Acceleration online-filtered','Acceleration offline-filtered');
+%Acceleration plot
+plot(time, acc_raw, time, acc_filt_on, time, acc_filt_off);
+title('Acceleration [deg/s^2] measured by Hall-Sensor raw / online-filtered / offline-filtered');
+xlabel('Time [s]');
+ylabel('Acceleration [deg/s^2]');
+legend('Acceleration raw','Acceleration online-filtered','Acceleration offline-filtered');
