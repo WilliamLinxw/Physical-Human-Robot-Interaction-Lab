a92389d0e05986b8efa23f407c31b9fdefc4dc28
TheB4ssPl4y3r <ramon.rohner@gmx.ch>
2021-11-02 14:09:57 +0100

    add final plot to report and packed report and Vis in zip-package

--- Lin_Rohner_lab2/motor_voltage_current_torque.m
+++ Lin_Rohner_lab2/motor_voltage_current_torque.m
@@ -1,4 +1,4 @@
-data = xlsread('motor_voltage_current.xlsx'); 
+data = xlsread('motor_voltage_current_torque.xlsx'); 
 voltage = data(:,1);
 current = data(:,2);
 torque = data(:,3);
@@ -7,19 +7,25 @@ torque = data(:,3);
 
 c1 = polyfit(voltage,current,1);
 disp(['Equation is current = ' num2str(c1(1)) '*voltage + ' num2str(c1(2))]);
-current_est = polyval(c1,voltage);
+current_est = polyval(c1, voltage);
 
 c2 = polyfit(voltage,torque,1);
-disp(['Equation is current = ' num2str(c2(1)) '*voltage + ' num2str(c2(2))]);
-torque_est = polyval(c2,voltage);
+disp(['Equation is torque = ' num2str(c2(1)) '*voltage + ' num2str(c2(2))]);
+torque_est = polyval(c2, voltage);
 
 
 yyaxis left
 plot(voltage, current);
+ylabel('Motor current [A]');
+xlabel('Input voltage [V]');
 hold on
-plot(voltage,current_est,'g--','LineWidth',2);
+plot(voltage,current_est,'b--','LineWidth',2);
 hold on
 yyaxis right;
-plot(voltage,torque)
+ylabel('Motor torque [mNm]');
+plot(voltage,torque);
 hold on
 plot(voltage,torque_est,'r--','LineWidth',2);
+hold on
+title('Motor current and motor torque dependent on input voltage');
+legend('Measured output [A] dependent on input [V]', 'Transfer function for motor current', 'Expected output [mNm] dependent on input [V]', 'Transfer function for motor torque', 'Location', 'southoutside', 'Orientation', 'vertical');
